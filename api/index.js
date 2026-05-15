cd76fa60dca27118d65b7469e1211abcda58ddb9
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 11:22:53 -0300

    chore: update shareCode to M8LKOAA7UN6

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=WMIOVMBY2I5&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=M8LKOAA7UN6&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
