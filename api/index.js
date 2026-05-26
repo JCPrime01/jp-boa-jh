bc997e4dbd93b58fd8fbb75854e52434c7f0a868
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 18:07:54 -0300

    chore: update shareCode to FTC8NVNTEAO

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=Q0TTN35632R&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=FTC8NVNTEAO&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
