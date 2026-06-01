f4643e16a79c90442d94a01768bbbdde9e6f46eb
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 14:47:59 -0300

    chore: shareCode 94SXL8FLG47 -> W5MXFZ6QEAR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=94SXL8FLG47&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=W5MXFZ6QEAR&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
